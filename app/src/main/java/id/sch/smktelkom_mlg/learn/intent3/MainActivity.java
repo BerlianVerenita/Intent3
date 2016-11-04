6154d7a7aa406f78ddca915bcbecaefed6ec8f47
BerlianVerenita <berlian_vebiola_23rpl@student.smktelkom-mlg.sch.id>
2016-11-04 20:52:24 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
                 dialPhoneNumber("0341712500");
             }
         });
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeSmsMessage("Pesan dari SMK Telkom Malang");
+            }
+        });
     }
         public void dialPhoneNumber(String phoneNumber) {
             Intent intent = new Intent(Intent.ACTION_DIAL);
@@ -26,5 +32,12 @@ public class MainActivity extends AppCompatActivity {
             if (intent.resolveActivity(getPackageManager()) != null)
                 startActivity(intent);
         }
+    public void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
     }
 
